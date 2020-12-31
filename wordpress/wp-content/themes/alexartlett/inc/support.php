26a9ddfaf9b48d1e11f937f581e7a9064c1898f3
Alex <artie1991@gmail.com>
2020-12-31 14:03:56 +1000

    Recursively get fields from work post type
    Exposes ACF Post Object to work acf fields

--- wordpress/wp-content/themes/alexartlett/inc/support.php
+++ wordpress/wp-content/themes/alexartlett/inc/support.php
@@ -3,3 +3,12 @@
 add_theme_support( 'custom-logo' );
 add_theme_support( 'menus' );
 add_theme_support('post-thumbnails');
+
+
+add_filter( 'acf/rest_api/recursive/types', function( $types ) {
+	if ( isset( $types['work'] ) ) {
+		unset( $types['work'] );
+	}
+
+	return $types;
+} );
