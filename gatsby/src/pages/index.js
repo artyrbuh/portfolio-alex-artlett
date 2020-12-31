7e104e562aa6a8be0f0f756959f17b78039bb830
Alex <artie1991@gmail.com>
2020-12-31 14:37:03 +1000

    Add css framework Bulma to app

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -3,6 +3,7 @@ import {graphql, StaticQuery, Link} from 'gatsby';
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import "../assets/styles/index.scss"
 
 const IndexPage = () => (
   <StaticQuery query={graphql`
@@ -57,15 +58,21 @@ const IndexPage = () => (
         console.log(props);
         return (
           <Layout>
-            <SEO title="Home" />
-            <h1>Hi people</h1>
-            <p>Welcome to your new Gatsby site.</p>
-            <p>Now go build something great.</p>
-            <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-              <Image />
+            <div className="container">
+              <div className="columns">
+                <div className="column">
+                  <SEO title="Home" />
+                  <h1>Hi people</h1>
+                  <p>Welcome to your new Gatsby site.</p>
+                  <p>Now go build something great.</p>
+                  <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+                    <Image />
+                  </div>
+                  <Link to="/page-2/">Go to page 2</Link> <br />
+                  <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+                </div>
+              </div>
             </div>
-            <Link to="/page-2/">Go to page 2</Link> <br />
-            <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
           </Layout>
           )
         }
