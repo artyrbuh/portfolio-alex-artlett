e3eb466e1b6f6fe1481bbf5a14533ee7f8c31051
Alex <artie1991@gmail.com>
2021-01-02 13:10:39 +1000

    Style offcanvas nav, add designation text to main menu list

--- gatsby/src/components/nav/offcanvas-menu.js
+++ gatsby/src/components/nav/offcanvas-menu.js
@@ -5,13 +5,17 @@ import Menu from '../main-menu/menu';
 const OffcanvasMenu = ({expanded}) => (
     <ThemeDataContext.Consumer>
         {themeData => {
-            console.log('expanded: ')
-            console.log(expanded)
+            const title = themeData.site.siteMetadata.title;
             return (
                 <div className={`offcanvas-menu ${expanded ? 'expanded' : ''}`}>
                     <div className="container">
-                        <div className="columns is-vcentered">
-                            <div className="column">
+                        <div className="columns is-vcentered is-flex">
+                            <div className="column wrap--main-menu">
+                                <div className="designation-col">
+                                    <div>
+                                        <p>{title}</p>
+                                    </div>
+                                </div>
                                 <Menu/>
                             </div>
                         </div>
