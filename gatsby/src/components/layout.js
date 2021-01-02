4a2a096ac72a9f48e3a67b3b23af575ec92fa472
Alex <artie1991@gmail.com>
2021-01-02 23:44:29 +1000

    remove template style

--- gatsby/src/components/layout.js
+++ gatsby/src/components/layout.js
@@ -59,13 +59,7 @@ const [themeData, setThemeData] = useState(ThemeData)
     <ThemeDataContext.Provider value={themeData}>
       <Nav/>
       <Contact/>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div>
         <main>{children}</main>
         <Footer/>
       </div>
