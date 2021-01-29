c8d3802d590683d748ff255ce0a738087aed6d45
Alex <artie1991@gmail.com>
2021-01-29 23:53:13 +1000

    Animate in the contact menu

--- gatsby/src/components/contact/contact-menu.js
+++ gatsby/src/components/contact/contact-menu.js
@@ -1,23 +1,52 @@
-import React, {useContext} from "react"
-import {ThemeDataContext} from '../layout';
+import React, {useContext, useRef, useState, useEffect} from "react"
+import {ThemeDataContext, ActiveMenu} from '../layout';
 import ContactMenuList from "./contact-menu-list";
+import { slideCloseMenuUp, slideOpenMenuDown } from "../../core/animation/menu";
 
 const ContactMenu = ({expanded}) => {
+    let menuRef = useRef(null);
+    let menuInner = useRef(null);
+    let menuInnerBG = useRef(null);
+    let [delay, setDelay] = useState(0);
+    const {activeMenu, mainMenuActive, setMainMenuActive, setContactMenuActive, initialClick} = useContext(ActiveMenu);
+
+    
+    useEffect(() => {
+        if(activeMenu === "contact") {
+            if(mainMenuActive === true) {
+                //setDelay(4.2);
+                setMainMenuActive(false);
+            }
+            slideOpenMenuDown(menuRef, menuInnerBG, menuInner, delay);
+        } else if((activeMenu === "" && initialClick == true) || (activeMenu == "main" && initialClick == true)) {
+            //close menu if no active menu or if active menu is contact menu
+            console.log('what');
+            setContactMenuActive(false);
+            slideCloseMenuUp(menuRef, menuInnerBG, menuInner);
+        }
+    }, [activeMenu]);
+
     const themeData = useContext(ThemeDataContext);
     const menu = themeData.allWordpressWpApiMenusMenusItems.edges[0].node.items;
     const email = themeData.wordpressAcfOptions.options.email;
 
     return (
-        <div className={`offcanvas-menu contact-menu ${expanded ? 'expanded' : ''}`}>
-            <div className="container wrap--contact-menu">
-                <div className="columns is-vcentered is-flex is-desktop menu">
-                    <ContactMenuList menu={menu}/>
-                </div>
+        <div 
+            className={`offcanvas-menu contact-menu ${expanded ? '' : ''}`}
+            ref={el => menuRef = el}
+        >
+            <div className="offcanvas-bg" ref={el => menuInnerBG = el}></div>
+            <div ref={el => menuInner = el} className="offcanvas-menu--inner">
+                <div className="container wrap--contact-menu">
+                    <div className="columns is-vcentered is-flex is-desktop menu">
+                        <ContactMenuList menu={menu}/>
+                    </div>
 
-                <a 
-                    href={`mailto:${email}`}
-                    className="email"
-                >{email}</a>
+                    <a 
+                        href={`mailto:${email}`}
+                        className="email"
+                    >{email}</a>
+                </div>
             </div>
         </div>
     );
