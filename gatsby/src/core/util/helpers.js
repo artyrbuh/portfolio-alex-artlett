2cc3dca11393d65491bba525d37b0565951c8ad0
Alex <artie1991@gmail.com>
2021-01-29 20:33:45 +1000

    Create extra helpers

--- gatsby/src/core/util/helpers.js
+++ gatsby/src/core/util/helpers.js
@@ -1,3 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+export function useWindowSize() {
+  function getSize() {
+    return {
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  }
+
+  const [windowSize, setWindowSize] = useState(getSize);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize(getSize());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
+}
+
+
 export const as_slug = (text) =>  text.toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'-');
 
 export const slugify_array = (items) => items.map(item => as_obj(item));
@@ -20,3 +45,22 @@ export const getMousePos = (e) => {
   }
   return { x: posx, y: posy };
 };
+
+
+// Linear interpolation
+export const lerp = (a, b, n) => (1 - n) * a + n * b;
+
+export const calcWinsize = () => {
+  return {width: window.innerWidth, height: window.innerHeight};
+};
+
+export const distance = (x1,y1,x2,y2) => {
+  var a = x1 - x2;
+  var b = y1 - y2;
+
+  return Math.hypot(a,b);
+}
+
+export const textAsSpans = (text) => {
+  return text.split('').map(el => <span>{el}</span>);
+}
