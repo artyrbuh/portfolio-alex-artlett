78d656612791f303de3d541689c1ced5dd6cf0f2
Alex <artie1991@gmail.com>
2021-01-29 23:50:52 +1000

    animate button state based on active menu

--- gatsby/src/components/ui/contact-button.js
+++ gatsby/src/components/ui/contact-button.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import Close from './close';
+import React, {useContext, useRef, useEffect} from 'react';
 import ContactText from './contact-text';
+import {ActiveMenu} from '../layout';
+import { scaleOutRotate, scaleInRotate, bgColourChange, staggerWidthsIn} from '../../core/animation';
 
 const ContactButton = ({expanded, onClick}) => {
+    const {disabled, mainMenuActive} = useContext(ActiveMenu);
+    let contactRef = useRef(null);
+    let closeRef = useRef(null);
+
+    useEffect(() => {
+        if(mainMenuActive) {
+            scaleOutRotate(contactRef);
+        }
+
+        if(!mainMenuActive) {
+            scaleInRotate(contactRef, .75);
+        }
+    }, [mainMenuActive]);
+
+    useEffect(() => {
+        if(expanded) {
+            bgColourChange(contactRef, "#fff", .3);
+            staggerWidthsIn(closeRef.children, .4);
+        } 
+
+        if(!expanded) {
+            bgColourChange(contactRef, "#000", .3, .4);
+        }
+    }, [expanded])
+
     return (
         <div 
-            className={`contact-button ${expanded ? 'open' : ''}`}
-            onClick={onClick}>
+            disabled={disabled}
+            className={`contact-button ${expanded ? '' : ''}`}
+            onClick={onClick}
+            ref={el => contactRef = el}
+        >
             <ContactText/>
-            <Close/>
+            <div className={`close`} ref={el => closeRef = el}>
+                <div></div>
+                <div></div>
+            </div>
         </div>
     );
 };
