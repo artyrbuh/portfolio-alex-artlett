ae0bf517c86bed7fc01a84d8e34d04eb0a64ac87
Alex <artie1991@gmail.com>
2021-01-29 19:03:23 +1000

    Create states and functions for animating in menus

--- gatsby/src/components/layout.js
+++ gatsby/src/components/layout.js
@@ -53,15 +53,55 @@ const Layout = ({ children, classes }) => {
   const [themeData] = useState(ThemeData)
   const [activeMenu, setActiveMenu] = useState("");
   const isMenu = (menu) => activeMenu === menu ? true : false;
-  const toggleMenu = (menu)  => isMenu(menu) ? setActiveMenu("") : setActiveMenu(menu);
+  
+  const [mainMenuActive, setMainMenuActive] = useState(false);
+  const [contactMenuActive, setContactMenuActive] = useState(false);
+
+  const [disabled, setDisabled] = useState(false);
+
+  const [initialClick, setInitialClick] = useState(false);
+
+  //Determine if menu button should be disabled
+  const disableMenu = () =>  {
+      setDisabled(!disabled);
+
+      setTimeout(() => {
+        setDisabled(false);
+      }, 1200)
+  }
+
+  const toggleMenu = (menu)  => { 
+    //only when this is set, will useEffect close menus
+    setInitialClick(true);
+    
+    //prevent user from clicking menu when menu is animating in
+    disableMenu();
+
+    /* Keep track of which menu is active */
+    if(menu === "main") {
+      setMainMenuActive(true);
+    }
+
+    if(menu === "contact") {
+      setContactMenuActive(true);
+    }
+
+    if(menu === "") {
+      setMainMenuActive(false);
+      setContactMenuActive(false);
+    }
+
+    //toggle the menu 
+    isMenu(menu) ? setActiveMenu("") : setActiveMenu(menu);
+  };
+
   const toggleMainMenu = () => toggleMenu("main");
   const toggleContactMenu = () => toggleMenu("contact");
-  
 
   return (
     <ThemeDataContext.Provider value={themeData}>
       <Cursor />
-      <ActiveMenu.Provider value={{toggleMainMenu, toggleContactMenu, isMenu}}>
+      <ActiveMenu.Provider value={{toggleMainMenu, toggleContactMenu, isMenu, activeMenu, mainMenuActive, contactMenuActive, disabled, initialClick}}>
         <Nav/>
         <Contact/>
       </ActiveMenu.Provider>
