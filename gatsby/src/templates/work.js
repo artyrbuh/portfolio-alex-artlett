2b9a208af2082826199aacf23e055b329e32bec3
Alex <artie1991@gmail.com>
2021-02-13 20:48:42 +1000

    Move video modal component, outside of skew scroll container

--- gatsby/src/templates/work.js
+++ gatsby/src/templates/work.js
@@ -20,28 +20,30 @@ export default ({pageContext}) => {
     return (
         <PageTransition>
             <WorkSingleContext.Provider value={{featured_media, slug, title, layouts_work, main_technology, professions, technologies, project_year, previous, next, project_website}}>
-                <WorkLayout classes={`work-single work-single--${slug}`}>
-                    <WorkBarNav>
-                        <AALink to={`/work`} className="back-cta">
-                            <svg 
-                                width="32" 
-                                height="32" 
-                                viewBox="0 0 32 32" 
-                                fill="none" 
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fillRule="evenodd" clipRule="evenodd" d="M11.7078 9.29202C11.801 9.38491 11.8748 9.49526 11.9253 9.61675C11.9757 9.73824 12.0016 9.86848 12.0016 10C12.0016 10.1316 11.9757 10.2618 11.9253 10.3833C11.8748 10.5048 11.801 10.6151 11.7078 10.708L6.41383 16L11.7078 21.292C11.8008 21.385 11.8746 21.4954 11.9249 21.6168C11.9752 21.7383 12.0011 21.8685 12.0011 22C12.0011 22.1315 11.9752 22.2617 11.9249 22.3832C11.8746 22.5047 11.8008 22.615 11.7078 22.708C11.6149 22.801 11.5045 22.8747 11.383 22.9251C11.2615 22.9754 11.1313 23.0013 10.9998 23.0013C10.8683 23.0013 10.7381 22.9754 10.6167 22.9251C10.4952 22.8747 10.3848 22.801 10.2918 22.708L4.29183 16.708C4.19871 16.6151 4.12482 16.5048 4.07441 16.3833C4.024 16.2618 3.99805 16.1316 3.99805 16C3.99805 15.8685 4.024 15.7382 4.07441 15.6167C4.12482 15.4953 4.19871 15.3849 4.29183 15.292L10.2918 9.29202C10.3847 9.19889 10.4951 9.125 10.6166 9.07459C10.7381 9.02418 10.8683 8.99823 10.9998 8.99823C11.1314 8.99823 11.2616 9.02418 11.3831 9.07459C11.5046 9.125 11.6149 9.19889 11.7078 9.29202Z" fill="black"/>
-                                <path fillRule="evenodd" clipRule="evenodd" d="M5 16C5 15.7348 5.10536 15.4804 5.29289 15.2929C5.48043 15.1054 5.73478 15 6 15H27C27.2652 15 27.5196 15.1054 27.7071 15.2929C27.8946 15.4804 28 15.7348 28 16C28 16.2652 27.8946 16.5196 27.7071 16.7071C27.5196 16.8946 27.2652 17 27 17H6C5.73478 17 5.48043 16.8946 5.29289 16.7071C5.10536 16.5196 5 16.2652 5 16Z" fill="black"/>
-                            </svg>  
-
-                            <h4>Back to my Selected work</h4>
-                        </AALink>
-                    </WorkBarNav>
-                    <WorkContainer>
-                        <WorkHeader/>
-                        <WorkLayouts/>
-                        <WorkFooterNav/>
-                    </WorkContainer>
-                </WorkLayout>
+                <VideoWrapper>
+                    <WorkLayout classes={`work-single work-single--${slug}`}>
+                        <WorkBarNav>
+                            <AALink to={`/work`} className="back-cta">
+                                <svg 
+                                    width="32" 
+                                    height="32" 
+                                    viewBox="0 0 32 32" 
+                                    fill="none" 
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M11.7078 9.29202C11.801 9.38491 11.8748 9.49526 11.9253 9.61675C11.9757 9.73824 12.0016 9.86848 12.0016 10C12.0016 10.1316 11.9757 10.2618 11.9253 10.3833C11.8748 10.5048 11.801 10.6151 11.7078 10.708L6.41383 16L11.7078 21.292C11.8008 21.385 11.8746 21.4954 11.9249 21.6168C11.9752 21.7383 12.0011 21.8685 12.0011 22C12.0011 22.1315 11.9752 22.2617 11.9249 22.3832C11.8746 22.5047 11.8008 22.615 11.7078 22.708C11.6149 22.801 11.5045 22.8747 11.383 22.9251C11.2615 22.9754 11.1313 23.0013 10.9998 23.0013C10.8683 23.0013 10.7381 22.9754 10.6167 22.9251C10.4952 22.8747 10.3848 22.801 10.2918 22.708L4.29183 16.708C4.19871 16.6151 4.12482 16.5048 4.07441 16.3833C4.024 16.2618 3.99805 16.1316 3.99805 16C3.99805 15.8685 4.024 15.7382 4.07441 15.6167C4.12482 15.4953 4.19871 15.3849 4.29183 15.292L10.2918 9.29202C10.3847 9.19889 10.4951 9.125 10.6166 9.07459C10.7381 9.02418 10.8683 8.99823 10.9998 8.99823C11.1314 8.99823 11.2616 9.02418 11.3831 9.07459C11.5046 9.125 11.6149 9.19889 11.7078 9.29202Z" fill="black"/>
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M5 16C5 15.7348 5.10536 15.4804 5.29289 15.2929C5.48043 15.1054 5.73478 15 6 15H27C27.2652 15 27.5196 15.1054 27.7071 15.2929C27.8946 15.4804 28 15.7348 28 16C28 16.2652 27.8946 16.5196 27.7071 16.7071C27.5196 16.8946 27.2652 17 27 17H6C5.73478 17 5.48043 16.8946 5.29289 16.7071C5.10536 16.5196 5 16.2652 5 16Z" fill="black"/>
+                                </svg> 
+                                <h4>Back to my Selected work</h4>
+                            </AALink>
+                        </WorkBarNav>
+                        <WorkContainer>
+                            <WorkHeader/>
+                            <WorkLayouts/>
+                            <WorkFooterNav/>
+                        </WorkContainer>
+                    </WorkLayout>
+                    <VideoModal/>
+                </VideoWrapper>
             </WorkSingleContext.Provider>
         </PageTransition>
     )
@@ -199,21 +201,64 @@ const WorkImagesBlock = ({data, i}) => {
     }
 }
 
-const WorkVideoBlock = ({data, i}) => {
+
+const VideoContext = createContext(null);
+
+const VideoWrapper = ({children}) => {
     const [modalLaunched, setModalLaunched] = useState(false);
     const [playing, setPlaying] = useState(false);
-
+    const [youtubeUrl, setYoutubeUrl] = useState(null);
 
     const handleClose = () => { 
         setModalLaunched(false);
         setPlaying(false);
     }
-    const handleShow = () => {
+    const handleShow = (youtube_url) => {
+        if(youtubeUrl === null) {
+            setYoutubeUrl(youtube_url);
+        }
+
         setModalLaunched(true);
         setPlaying(true);
     }
 
+    return (
+        <VideoContext.Provider value={{modalLaunched, setModalLaunched, handleClose, handleShow, playing, setPlaying, youtubeUrl, setYoutubeUrl}}>
+            {children}
+        </VideoContext.Provider>
+    );
+}
+
+const VideoModal = () => {
+    const {modalLaunched, handleClose, playing, youtubeUrl} = useContext(VideoContext);
+
+    return (
+        <Modal 
+            isActive={modalLaunched} 
+            closeMenu={handleClose}
+            classes={`video-modal`}>
+            <header className="modal-card-head">
+                <div onClick={handleClose} className="close-modal">
+                    <Close/>
+                </div>
+            </header>
+            <section className="modal-card-body">
+                <ReactPlayer 
+                    className="video-modal--video-player"
+                    controls={true}
+                    url={youtubeUrl}
+                    playing={playing}
+
+                />
+            </section>
+        </Modal>
+    );
+}
+
+const WorkVideoBlock = ({data, i}) => {
     const {caption, include_available_for_hire_cta, video_preview, youtube_url} = data;
+    const {handleShow} = useContext(VideoContext);
+
     return (
         <>
             <div className="work-block work-video-block">
@@ -223,7 +268,7 @@ const WorkVideoBlock = ({data, i}) => {
 
                 <div 
                     className="work-video-block--preview"
-                    onClick={handleShow}
+                    onClick={() => handleShow(youtube_url)}
                 >
                     <img src={video_preview.source_url} />
                     <svg className="play-btn" x="0px" y="0px" width="408.221px" height="408.221px" viewBox="0 0 408.221 408.221" >
@@ -238,29 +283,6 @@ const WorkVideoBlock = ({data, i}) => {
                     />
                 )}                
             </div>
-
-            <Modal 
-                isActive={modalLaunched} 
-                closeMenu={handleClose}
-                classes={`video-modal`}>
-                <header className="modal-card-head">
-                    <div 
-                        onClick={handleClose} 
-                        className="close-modal"
-                    >
-                        <Close/>
-                    </div>
-                </header>
-                <section className="modal-card-body">
-                    <ReactPlayer 
-                        className="video-modal--video-player"
-                        controls={true}
-                        url={youtube_url}
-                        playing={playing}
-
-                    />
-                </section>
-            </Modal>
         </>
     );
 }
