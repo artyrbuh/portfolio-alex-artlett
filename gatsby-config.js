3ccf9ba601c65a3b0d05bbbcfe8df27e6af26a7f
Alex <artie1991@gmail.com>
2021-02-21 15:23:06 +1000

    add netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,7 @@ module.exports = {
         icon: `src/assets/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-netlify`,
     {
       resolve: "gatsby-source-wordpress",
       options: {
